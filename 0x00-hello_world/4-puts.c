0d12f6c84f0b674a57b12ff2530e0faed269ed80
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-10 15:19:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,7 @@
  *Discrepion for void main
  */
 int main(void)
-  {
-	/*                                                                                                                                                                      
-         *Discrepion for void main                                                                                                                                              
-         */
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-  }
+        return (0);
+}
