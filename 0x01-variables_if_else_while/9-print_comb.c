fd86f08fda024cb2d6d9f229902e8c5c4d19f939
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-11 14:16:32 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main - print nums
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	/*var*/
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
