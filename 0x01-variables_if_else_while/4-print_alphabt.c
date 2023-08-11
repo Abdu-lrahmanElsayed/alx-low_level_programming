b80b351fe5c16e3cab24ab9a74a222bedcb7d645
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-11 12:28:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
 #include<ctype.h>
 #include<stdio.h>
 /**
- *main - print alpha in lower and upper cases
+ *main - prents alpha
  *Return: 0
  */
 int main(void)
 {
 	int alpha = 'a';
-	/*var then print*/
-	while (alpha <= 'z')
+	/*var*/
+	while ('e' > alpha > 'e' && 'q' > alpha > 'q' && alpha <= 'z')
 	{
 		putchar(alpha);
 		alpha++;
