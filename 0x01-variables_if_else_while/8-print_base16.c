01fbe3cb4aa3a5d545e1c1430b99396aba73feee
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-11 13:52:37 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - print nums
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	/*var*/
+	while (i < 16)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
