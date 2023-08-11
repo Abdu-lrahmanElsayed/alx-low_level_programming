c5be298349ce287c01b892e5b5a995517d076418
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-11 14:41:07 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ *main - print nums
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	/*var*/
+	while (i < 90)
+	{
+		putchar(i + '1');
+		if (i < 89)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
