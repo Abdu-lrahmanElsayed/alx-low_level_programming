9c9967a2b0834da1499cd30e5deff63e170055cb
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-14 13:54:30 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,11 +21,11 @@ int main(void)
 				putchar (j + '0');
 				putchar (k + '0');
 				if (i != 7 || j != 8 || k != 9)
-					{
-						putchar (',');
-						putchar (' ');
-					}
-					k++;
+				{
+					putchar (',');
+					putchar (' ');
+				}
+				k++;
 			}
 			j++;
 		}
