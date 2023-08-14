65323760d3e8b0a7d0b3751989e8494d59d72674
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-14 13:46:03 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ *main - print nums
+ *Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	/*var*/
+	while (i <= 9)
+	{
+		j = i + 1;
+		while (j <= 9)
+		{
+			while
+			{
+				putchar (i + '0');
+				putchar (j + '0');
+				putchar (k + '0');
+				if (i != 7 || j != 8 || k != 9)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+					k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
