15167b5f816e14dd30d95868aa755701127b51f2
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-18 12:09:26 -0700

    task0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,4 @@
 #ifndef MAIN.H
 #define MAIN.H
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-	{
-		printf("1");
-	}
-	else if (c >= 'a' && c <= 'z')
-	{
-		printf("0");
-	}
-}
+int _isupper(int c);
 #endif
