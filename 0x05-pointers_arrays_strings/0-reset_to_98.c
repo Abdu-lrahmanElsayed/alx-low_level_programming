a8ab50bea437131cc8cc94b909becaa405312b89
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-21 12:38:22 -0700

    betty f***

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,8 @@
  *reset_to_98 - update the value to 98
  *Reaturn: *n value;
  */
-void reset_to_98(int *n)/*integer pointer n*/
+void reset_to_98(int *n)
+/*integer pointer n*/
 {
        	*n = 98;
 }
