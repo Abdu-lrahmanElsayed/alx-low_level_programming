b84fbfc9e581ea29e300cc07ced255d53359c72c
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-27 12:30:46 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,3 +4,15 @@
  * print_alphabet - prints alphabets
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+	int i = 'a';
+	/*var then while loop*/
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
