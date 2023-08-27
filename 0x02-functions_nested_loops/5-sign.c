2fd3cecfbee46a51dbaa82f7911c15729575c56d
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-27 14:39:51 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+\n");
+		printf("+");
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
+		printf("0");
 		return (0);
 	}
 	else
-		printf("-\n");
+		printf("-");
 	return (-1);
 }
