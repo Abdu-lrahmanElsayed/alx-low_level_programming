3c1b6d6faec3a320cfc1b12cf284ccbd99902768
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-28 13:35:29 -0700

    ld

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *print_last_digit - '_'
+ *Return: ld
+ */
+int print_last_digit(int)
+{
+	int ld;
+	ld = int % 10;
+	printf("%d", ld);
+}
