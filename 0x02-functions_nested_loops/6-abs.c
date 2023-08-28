dcc95e5c53040cb0d05e98758873709052a7f3a7
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-28 13:24:52 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
-#inculde "main.h"
+#include "main.h"
 #include<stdlib.h>
 #include<stdio.h>
 /**
  *_abs - prints abs value of an integer
+ *@n: input
  *Return: abs value
  */
-int _abs(int)
+int _abs(int n)
 {
 	printf("%u", int);
 }
