995c2aaea7432d24a1db3a8dc4af30506842d1e1
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-28 13:52:39 -0700

    ld

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,14 @@
 #include<stdlib.h>
 /**
  *print_last_digit - '_'
+ *@n: input
  *Return: ld
  */
 int print_last_digit(int n)
 {
 	int ld;
+	/*variable to git last digit*/
 	ld = n % 10;
+	printf("%d", ld);
 	return (ld);
 }
