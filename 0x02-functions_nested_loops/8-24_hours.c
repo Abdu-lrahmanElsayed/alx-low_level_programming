c2891678470960c1606b5baede48397714a06027
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-28 14:50:48 -0700

    timr

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *jack_bauer - prints very min and hour in the day
+ *Return: time
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	/*int var for time*/
+	while (i < 3)
+	{
+		putchar(i + '0');
+		while (j < 10)
+		{
+			putchar(j + '0');
+			putchar(":");
+			while (k < 6)
+			{
+				putchar(k + '0');
+				while (l < 10)
+				{
+					putchar(l + '0');
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+}
