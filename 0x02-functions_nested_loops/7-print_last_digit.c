649c2318ea2262fb265e93a73fc66aea9721a36d
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-28 14:01:10 -0700

    a

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,14 @@ int print_last_digit(int n)
 {
 	int ld;
 	/*variable to git last digit*/
-	ld = n % 10;
+	if (n < 0)
+	{
+		ld = -n % 10;
+		printf("%d", ld);
+		return (ld);
+	}
+	else
+		ld = n % 10;
 	printf("%d", ld);
 	return (ld);
 }
