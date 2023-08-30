f430c305646cc471a75b6e7053ac03c86f534d86
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-30 13:23:56 -0700

    l

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *_strlen_recursion - prints legnth of string
+ *@s: input string
+ *Return: legnth
+ */
+int _strlen_recursion(char *s)
+{
+	int l;
+	/*length*/
+	l = strlen(s);
+	printf("%d", l);
+}
