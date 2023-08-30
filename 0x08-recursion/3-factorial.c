3a0e0b3727ea545da48fe1f0b89f1e0491ee477e
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-30 13:38:58 -0700

    fac

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *factorial - prints factorial
+ *@n: a num
+ *Return: fac
+ */
+int factorial(int n)
+{
+	int i ,j;
+	/*variabls for fac*/
+	i = 1;
+	j = 1;
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		while (i <= n)
+		{
+			j = j * i;
+			i++;
+		}
+		return (j);
+	}
+}
