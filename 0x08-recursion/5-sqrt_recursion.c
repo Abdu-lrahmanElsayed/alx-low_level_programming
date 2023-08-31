cc228fb35cad17de858c531faa68af495741380d
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-31 15:04:32 -0700

    sq

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#enclude "main.h"
+#include<stdio.h>
+#include<math.h>
+/**
+ *_sqrt_recursion - gets squer of a num
+ *@n: integer input
+ *Return: square
+ */
+int _sqrt_recursion(int n)
+{
+	if (n % n != 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * _sqrt_recursion(n - 1));
+	}
+}
