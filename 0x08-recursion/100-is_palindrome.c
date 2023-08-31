e76129336270af33b8bb33a1c5aacabd8617d6da
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-08-31 16:02:35 -0700

    pal

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *is_palindrome - check string
+ *@s: input string
+ *Return: 0 or 1
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
