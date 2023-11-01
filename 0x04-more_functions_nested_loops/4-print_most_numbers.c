28bdf1b32b5accd1d21d0e0d2726baefae48eccc
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-11-01 14:40:04 -0700

    update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 	
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i == 2 || i==4)
-			i++;
-		else
-			_putchar(i + '0');
-		i++;
+			continue;
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
