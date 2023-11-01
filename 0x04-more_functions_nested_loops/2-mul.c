44bc9d35703afd28c4d693675f2c0d7cce2ab938
Abdulrahman <abdulrahmanelsid@gmail.com>
2023-11-01 13:58:05 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies tow integers
+ * @a: 1st input int
+ * @b: 2nd input int
+ * Return: multiput result
+ */
+int mul(int a, int b)
+{
+	int mul_result;
+
+	mul_result = a * b;
+	return (mul_result);
+}
