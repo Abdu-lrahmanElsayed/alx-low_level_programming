07368f352b7cd2202eacbee50d7580e1e402c62c
Abdulrahman <Abdulrahman>
2024-01-23 14:56:03 -0800

    0.0.2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,25 +16,13 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (a > c && c > b)
-    {
-        largest = a;
-    }
     else if (b > a && a > c)
     {
         largest = b;
     }
-    else if (b > c && c > a)
-    {
-        largest = b;
-    }
-    else if (c > a && a > b)
+    else
     {
         largest = c;
-    }
-    else if (c > b && b > a)
-    {
-	    largest = c;
     }
 
     return (largest);
