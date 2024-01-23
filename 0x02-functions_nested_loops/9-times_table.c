ae49f9768a7e4bac4f81ac734d80b7fa4f8ba7fc
Abdulrahman <Abdulrahman>
2024-01-23 09:26:25 -0800

    test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,17 +6,16 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i = 0;
+	int j;
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 10; i++)
+		while (i <= 9)
 		{
 			_putchar(i + j + '0');
-			if (i < 9)
-			{
-				_putchar(',');
-			}
+			_putchar(',');
+			i++;
 		}
 		_putchar('\n');
 	}
