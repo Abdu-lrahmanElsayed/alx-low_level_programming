81f077f3f2baa8519c30721224add75ed8d629dc
Abdulrahman <Abdulrahman>
2024-01-28 05:10:32 -0800

    '_'

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,19 @@
 void more_numbers(void)
 {
 	int i = 0;
-	int j;
+	int j, k;
 
 	while (i < 10)
 	{
 		j = 0;
 		while (j <= 14)
 		{
+			j = k;
+			if (k > 9)
+			{
+				_putchar(1 + '0');
+				j = k % 10;
+			}
 			_putchar(j + '0');
 			j++;
 		}
