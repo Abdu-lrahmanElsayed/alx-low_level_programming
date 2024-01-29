8b01203c5d98b5d473626d3e31dbce7f10af7387
Abdulrahman <Abdulrahman>
2024-01-29 13:27:09 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string.
+ *@s: input pointer string type
+ *
+ * Return: legnth of the string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	for (length = 0; *s != '\0'; s++)
+		++legnth;
+	return (length);
+}
