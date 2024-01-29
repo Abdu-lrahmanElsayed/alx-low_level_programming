02d3006418f7acc9434a8540c9e50b984739c923
Abdulrahman <Abdulrahman>
2024-01-29 13:37:07 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: input pointer string.
+ */
+
+void _puts(char *str)
+{
+	char mystr = *str;
+	fprintf(stdout,"%c",mystr);
+	putchar('\n');
+}
