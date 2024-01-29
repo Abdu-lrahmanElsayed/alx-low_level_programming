66840b689d2c64af7f767bf8dd295420e09e57f3
Abdulrahman <Abdulrahman>
2024-01-29 13:49:36 -0800

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,14 +7,8 @@
 
 void _puts(char *str)
 {
-	int length;
+	char mystr[] = *str;
 
-	for (length = 0; *str != '\0'; str++)
-		++length;
-
-	char mystr[length];
-
-	mystr = *str;
 	fprintf(stdout,"%s",mystr);
 	putchar('\n');
 }
