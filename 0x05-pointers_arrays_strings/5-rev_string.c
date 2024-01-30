0bf5f15f471e9f09248abfaf07f0afca6e23c7fa
Abdulrahman <Abdulrahman>
2024-01-30 03:45:41 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input pointer string.
+ */
+
+void print_rev(char *s)
+{
+	int length, i;
+
+	for (length = 0; *s != '\0'; s++)
+		++length;
+	i = length - 1;
+	while (i >= 0)
+	{
+		putchar(s[i] + '0');
+		i--;
+	}
+}
