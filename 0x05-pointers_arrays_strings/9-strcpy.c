12ce5ea2dc841d46175112b7f4870dadd1f252ff
Abdulrahman <Abdulrahman>
2024-01-30 10:58:19 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,10 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = -1;
-	do
-	{
+	do {
 		++i;
 		dest[i] = src[i];
-	}while (src[i] != '\0');
+	} while (src[i] != '\0');
 
 	return (dest);
 }
