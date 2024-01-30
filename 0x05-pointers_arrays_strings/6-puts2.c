5392d2ceb5347dcbb4bae459ea69392cb60ad275
Abdulrahman <Abdulrahman>
2024-01-30 03:52:08 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character starting with the first character.
+ * @str: input pointer string
+ */
+
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str+=2;
+	}
+	putchar('\n');
+}
