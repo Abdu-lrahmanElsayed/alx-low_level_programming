8d5a29525672e71b7b1f94fdc20dd1cbe2585c01
Abdulrahman <Abdulrahman>
2024-02-01 12:44:20 -0800

    tsk 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
+
 	if (size == 0 || n == 0)
 		return (0);
 
 	while (size--)
-		n[size - 1] = c;
-	return (c);
+		n[size] = c;
+
+	return (n);
 }
