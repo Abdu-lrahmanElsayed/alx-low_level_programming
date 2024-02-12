3b6c7236223154b2e18a06371369d4bc94078963
Abdulrahman <Abdulrahman>
2024-02-12 12:13:53 -0800

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int n, num;
+	int num0 = 0;
+	int num1 = 1;
+	
+	for (n = 1; n <= 98; ++n)
+	{
+		printf("%d, ", num1);
+		num = num0 + num1;
+		num0 = num1;
+		num1 = num;
+	}
+	return (0);
+}
