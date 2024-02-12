94a22989f10ff8bc582ac391e5405f119e43d4a5
Abdulrahman <Abdulrahman>
2024-02-12 10:58:59 -0800

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,17 @@
 void natural_3_5(void)
 {
 	int i = 0;
+	int sum;
 
 	while (i < 1024)
 	{
-		int sum = 0;
+		sum = 0;
 
 		if (i % 3 != 0 || i % 5 != 0)
 			sum += i;
 		else
 			continue;
+		i++;
 	}
 	_putchar(sum + '0');
 	_putchar('\n');
