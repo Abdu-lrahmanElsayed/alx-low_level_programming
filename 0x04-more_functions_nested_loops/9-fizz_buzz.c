0cf3960bff19e1279c81dd0a46ed872cbeba8d85
Abdulrahman <Abdulrahman>
2024-02-14 12:51:55 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,26 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (n < 100)
 		{
-			printf("FizzBuzz");
+			if ((n % 3 == 0) && (n % 5 == 0))
+			{
+				printf("FizzBuzz");
+			}
+			else if (n % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			else if (n % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			else
+				printf("%d", n);
+			printf(" ");
 		}
-		else if (n % 3 == 0)
-		{
-			printf("Fizz");
-		}
-		else if (n % 5 == 0)
-		{
+		else if (n == 100)
 			printf("Buzz");
-		}
-		else
-			printf("%d", n);
-		printf(" ");
 		n++;
 	}
 	putchar('\n');
