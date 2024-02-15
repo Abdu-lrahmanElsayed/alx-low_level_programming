430d1de929fdf67b82a6fcd04cbabaf05b9ffb8b
Abdulrahman <Abdulrahman>
2024-02-15 11:12:39 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,11 @@ void rev_string(char *s)
 	int length, i;
 
 	for (length = 0; *s != '\0'; s++)
-		++length;
+		length++;
 	i = length - 1;
 	while (i >= 0)
 	{
-		putchar(s[i] + '0');
+		_putchar(s[i]);
 		i--;
 	}
 }
