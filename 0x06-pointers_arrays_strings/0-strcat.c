fd649707a8672ba9ef2865af758df0ac7e396d1c
Abdulrahman <Abdulrahman>
2024-02-16 10:56:06 -0800

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat: adds array2 to array1
+ * *_strcat - adds array2 to array1
  * @dest: input string
  * @src: input string
  * Return: dest
@@ -12,12 +12,11 @@ char *_strcat(char *dest, char *src)
 	int j;
 	int i = 0;
 
-	while(dest[i])
+	while (dest[i])
 		i++;
 	for (j = 0; src[j]; j++)
 	{
 		dest[i++] = src[j];
 	}
-
 	return (dest);
 }
