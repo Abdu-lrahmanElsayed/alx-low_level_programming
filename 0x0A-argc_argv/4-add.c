513f5b38655ab8c224f97fe2a20e6b3467d2f521
Abdulrahman <Abdulrahman>
2024-02-21 12:48:32 -0800

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,21 +14,24 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	else if (argc >= 2)
+	else
 	{
-		int i = 0;
+		int i, j;
 		int add = 0;
 
-		while (i < argc)
+		for (i = 1; i < argc; i++)
 		{
-			add = atoi(argv[i]);
-			add += add;
-			i++;
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[i]);
 		}
 		printf("%d\n", add);
-		return (0);
 	}
-	else
-		printf("Error\n");
-	return (1);
+	return (0);
 }
