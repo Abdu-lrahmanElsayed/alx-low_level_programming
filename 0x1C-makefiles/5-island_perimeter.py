73cf647823b9f66d4614b2559fa7c7acedd593b3
Abdu <Abdu>
2024-06-05 10:54:23 -0700

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            if grid[i][j] == 1:
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
